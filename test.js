2112f854eb6fc02738331b6fe238f034c5ca2706
tasnimul2 <chowdhury1478@gmail.com>
2022-01-12 23:09:38 -0500

    heroku down

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
-const {client_id, client_secret} = require('./auth/credentials.json');
+require('dotenv').config();
+let client_id = process.env.client_id;
+let client_secret = process.env.client_secret;
 
 let base64data = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
 console.log(`Basic ${base64data}`);
